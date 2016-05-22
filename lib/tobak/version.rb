b509f5ee879e1a0f3ac29ede35783056b10bb1e8
Thilo Fischer <thilo-fischer@gmx.de>
2016-05-22 14:32:37 +0200

    initial project setup

---
+++ lib/tobak/version.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016  Thilo Fischer.
+#
+# This file is part of tobak.
+#
+# tobak is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# tobak is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with tobak.  If not, see <http://www.gnu.org/licenses/>.
+
+module Tobak
+
+  #--
+  # XXX use git describe or ident (git attributes)? ... http://stackoverflow.com/questions/1792838/how-do-i-enable-ident-string-for-git-repos
+  VERSION = '0.0.1'
+
+end # module Tobak
